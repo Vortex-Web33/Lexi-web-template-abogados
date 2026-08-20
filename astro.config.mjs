b827fac0f3b99624cd8e1835e33485abc8825eb8
Ruben <neburtl837@gmail.com>
2026-08-20 20:55:33 +0200

    feat: aro de 4px del color del fondo alrededor del medallón

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
   trailingSlash: 'never',
   integrations: [
     sitemap({ i18n: { defaultLocale: 'en', locales: { en: 'en-US', es: 'es-ES' } } }),
-    icon({ include: { lucide: ['arrow-up-right', 'badge-check', 'mail', 'x'] } }),
+    icon({ include: { lucide: ['arrow-down', 'arrow-up-right', 'badge-check', 'mail', 'x'] } }),
   ],
   vite: {
     plugins: [tailwindcss()],
